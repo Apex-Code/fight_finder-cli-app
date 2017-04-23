6bbac0a90cb835762eecafbc2bf279c297e3ae62
David Mars <david.w.mars@my.occc.edu>
2017-04-23 04:30:24 +0000

    start scraper, def get_upcoming

--- lib/fight_finder/scraper.rb
+++ lib/fight_finder/scraper.rb
@@ -1,7 +1,14 @@
 #   ~~~HAVICK WAS HERE~~~
-
+require 'open-uri'
+require 'pry'
+require 'nokogiri'
 
 class Scraper
-
-
+def self.get_upcoming
+	doc = Nokogiri::HTML(open("http://www.ufc.com/schedule/event"))
+	binding.pry
+	 doc
 end
+end
+
+Scraper.get_upcoming
